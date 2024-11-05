2c751d8ae6c08115e3877b2bd93624fb42b28b21
tauheed0110 <tauheed0110@gmail.com>
2024-11-05 15:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const counter = document.getElementById("counter");
+
+function handleCounter(){
+    const num = parseInt(counter.textContent);
+    alert(num);
+    counter.textContent = num + 1;
+}
